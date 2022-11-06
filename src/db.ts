607fc6e13b4814ae08beac5c574956ad10ba1427
Dmitry Moskalenko <dimamikron41@gmail.com>
2022-11-06 13:36:30 +0300

    test heroku

--- src/db.ts
+++ src/db.ts
@@ -2,18 +2,25 @@ import { Sequelize } from 'sequelize';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-const { DATABASE } = process.env;
+// local
+// const { DATABASE } = process.env;
 
-const seqDataBase = new Sequelize('lang4u', 'postgres', 'postgres', {
-    dialect: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    // dialectOptions: {
-    //     ssl: {
-    //         require: true,
-    //         rejectUnauthorized: false,
-    //     },
-    // },
+// const seqDataBase = new Sequelize('lang4u', 'postgres', 'postgres', {
+//     dialect: 'postgres',
+//     host: 'localhost',
+//     port: 5432,
+
+// });
+
+// heroku server postgresql
+const { DATABASE } = process.env;
+const seqDataBase = new Sequelize(DATABASE!, {
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        },
+    },
 });
 
 export default seqDataBase;
